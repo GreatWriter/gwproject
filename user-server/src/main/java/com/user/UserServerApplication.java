32838af50c3126b3cc8b1aefb1b7e2e5f5a7a81c
GreatWriter <jason.galaxy.s8@gmail.com>
2018-02-24 16:54:03 +0800

    Initial commit

---
+++ user-server/src/main/java/com/user/UserServerApplication.java
@@ -0,0 +1,22 @@
+package com.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class UserServerApplication {
+
+	@RequestMapping(value = "/userlogin")
+	public String usertest(){
+		return "user test";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServerApplication.class, args);
+	}
+}
